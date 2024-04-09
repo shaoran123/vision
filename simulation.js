a252ca14359f11dfd37e619aceafd85412ceb841
Shaoran Liang <lians745@newschool.edu>
2024-04-09 13:42:15 -0400

    Update on AMD vision and some css

--- simulation.js
+++ simulation.js
@@ -2,41 +2,91 @@ console.log("hi shaoran");
 
 //state
 let animationActive = false;
+let activeLight = null;
 
 //get elements from html dom
 const glaucoma = document.getElementById('glaucoma');
+const AMD = document.getElementById('AMD');
 const stopBtn = document.getElementById('stop');
 const glaucomaLight = document.getElementById('glaucomaLight');
+const AMDLight = document.getElementById('AMDLight');
 
 //mouse position
-let pos = document.documentElement;
-pos.addEventListener('mousemove', e =>{
-	pos.style.setProperty('--x', e.clientX + 'px');
-	pos.style.setProperty('--y', e.clientY + 'px');
-});
 
-//click
+
+
+
 // glaucoma.onclick = () => {
-// 	if (animationActive == true) {
+// 	if (animationActive == false) {
+// 		glaucomaLight.style.display = 'block';
+// 		animationActive = true;
+// 	} else {
 // 		glaucomaLight.style.display='none';
 // 		animationActive = false;
-// 	} else {
-// 		glaucomaLight.style.display='block';
-// 		animationActive = true;
 // 	}
 // }
+
+// AMD.onclick = () =>{
+// 	if (animationActive == false) {
+// 		AMDLight.style.display = 'block';
+// 		animationActive = true;
+// 	} else {
+// 		AMDLight.style.display='none';
+// 		animationActive = false;
+// 	}
+// }
+
+// //stop
+// stopBtn.onclick = () => {
+// 	animationActive = false;
+// 	glaucomaLight.style.display='none';
+// 	AMDLight.style.display='none';
+// }
+
+// Glaucoma click event
 glaucoma.onclick = () => {
-	if (animationActive == false) {
-		glaucomaLight.style.display = 'block';
-		animationActive = true;
-	} else {
-		glaucomaLight.style.display='none';
-		animationActive = false;
-	}
+    if (activeLight != glaucomaLight) {
+		closeAll()
+        glaucomaLight.style.display = 'block';
+        activeLight = glaucomaLight;
+        animationActive = true;
+
+		document.addEventListener('mousemove', e => {
+			glaucomaLight.style.setProperty('--x', e.clientX + 'px');
+			glaucomaLight.style.setProperty('--y', e.clientY + 'px');
+		});
+    } else {
+		closeAll();
+    }
 }
 
-//stop
-stopBtn.onclick = () => {
+// AMD click event
+AMD.onclick = () => {
+    if (activeLight != AMDLight) {
+		closeAll()
+        AMDLight.style.display = 'block';
+        activeLight = AMDLight;
+        animationActive = true;
+
+		document.addEventListener('mousemove', e =>{
+			console.log('Moving AMDLight');
+			AMDLight.style.left = e.clientX-(AMDLight.clientWidth/2.5) + 'px';
+			AMDLight.style.top = e.clientY-(AMDLight.clientHeight/2.5) + 'px';
+		});
+
+    } else {
+		closeAll();
+    }
+}
+
+function closeAll() {
+	AMDLight.style.display = 'none';
+	glaucomaLight.style.display = 'none';
+	activeLight = null;
 	animationActive = false;
-	glaucomaLight.style.display='none';
+}
+
+// Stop click event
+stopBtn.onclick = () => {
+    closeAll();
 }
